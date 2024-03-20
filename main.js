044066d3581e5ac5d8bdecff7ffc563ae70ad312
Ulises Ruggeri <ruggeriulises2@gmail.com>
2024-03-20 19:00:38 -0300

    Portfolio3

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-/*prueba de git */
-/* */
+"use strict";
+
+document.querySelectorAll("li").forEach(function (li) {
+  li.addEventListener("click", function () {
+    const target = this.getAttribute("target");
+    const article = document.getElementById(target);
+    article.scrollIntoView({ behavior: "smooth" });
+  });
+});
