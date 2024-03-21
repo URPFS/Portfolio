0afc88097e1c53e5a34a81dfdb7c0d22a3b32cbb
Ulises Ruggeri <ruggeriulises2@gmail.com>
2024-03-21 18:41:10 -0300

    Portfolio4

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const cabeza = document.getElementById("cabeza");
+const headerHeight = cabeza.offsetHeight;
+let lastScrollY = window.scrollY;
 document.querySelectorAll("li").forEach(function (li) {
   li.addEventListener("click", function () {
     const target = this.getAttribute("target");
@@ -7,3 +10,20 @@ document.querySelectorAll("li").forEach(function (li) {
     article.scrollIntoView({ behavior: "smooth" });
   });
 });
+
+function updateHeaderPosition() {
+  const scrollY = window.scrollY;
+  if (scrollY > lastScrollY) {
+    if (scrollY >= headerHeight) {
+      cabeza.style.position = "fixed";
+      cabeza.style.top = "0";
+    }
+  } else {
+    if (scrollY <= headerHeight) {
+      cabeza.style.position = "relative";
+    }
+  }
+  lastScrollY = scrollY;
+}
+
+window.addEventListener("scroll", updateHeaderPosition);
